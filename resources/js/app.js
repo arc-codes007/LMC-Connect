ee2405694c1a3e1a8175501d625a3a1b79f56be6
Utkarsh Bhati <utkarshbhati2000@gmail.com>
2021-08-21 14:56:33 +0530

    Add post to + icon Added tinymce

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,3 +25,49 @@ $.ajaxSetup({
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+
+  require('tinymce');
+  require('tinymce/themes/silver');
+  require('tinymce/icons/default');
+  require('tinymce/plugins/paste');
+  require('tinymce/plugins/advlist');
+  require('tinymce/plugins/autolink');
+  require('tinymce/plugins/lists');
+  require('tinymce/plugins/link');
+  require('tinymce/plugins/image');
+  require('tinymce/plugins/charmap');
+  require('tinymce/plugins/print');
+  require('tinymce/plugins/preview');
+  require('tinymce/plugins/anchor');
+  require('tinymce/plugins/textcolor');
+  require('tinymce/plugins/searchreplace');
+  require('tinymce/plugins/visualblocks');
+  require('tinymce/plugins/code');
+  require('tinymce/plugins/fullscreen');
+  require('tinymce/plugins/insertdatetime');
+  require('tinymce/plugins/media');
+  require('tinymce/plugins/table');
+  require('tinymce/plugins/contextmenu');
+  require('tinymce/plugins/code');
+  require('tinymce/plugins/help');
+  require('tinymce/plugins/wordcount');
+  require('tinymce/plugins/autoresize');
+
+  $( document ).ready(function() {
+    tinymce.init({
+        selector: ".tiny",
+        // height: 300,
+        menubar: false,
+        inline: true,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
+            'autoresize'
+        ],
+        autoresize_on_init: true,
+        toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+        branding: false,
+    });
+});
